24356d843e56e2309146dd78f204777dd36e4845
devid.farinelli@gmail.com <devid.farinelli@gmail.com>
2020-01-23 03:32:58 +0100

    (ui) adds content & bottom navigation

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
+import { BottomNavigation, BottomNavigationAction } from '@material-ui/core';
+import PersonIcon from '@material-ui/icons/Person';
+import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered';
+import SportsEsportsSharpIcon from '@material-ui/icons/SportsEsportsSharp';
+
+import Game from './Game/Game'
 
 function App() {
+  let page = 0
+
+  const switchPage = (e, newPage) => {
+    page = newPage
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FullscreenWrapper>
+      <Content>
+
+      </Content>
+      <Navigation value={page} onChange={switchPage} showLabels>
+        <BottomNavigationAction icon={<FormatListNumberedIcon />} />
+        <BottomNavigationAction icon={<SportsEsportsSharpIcon />} />
+        <BottomNavigationAction icon={<PersonIcon />} />
+      </Navigation>
+    </FullscreenWrapper>
   );
 }
+
+const FullscreenWrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+`
+
+const Navigation = styled(BottomNavigation)`
+  bottom: 0;
+`
+
+const Content = styled.div`
+  height: 100%;
+  background-color: green;
+`
 
 export default App;
---
+++ src/Game/Game.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Game() {
+    return <h1>I'm a Game</h1>
+}
+
+export default Game;
